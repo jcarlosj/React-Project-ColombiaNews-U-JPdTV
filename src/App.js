75d92366b2f9494a99cd9d9338c74d3c71c1a5e7
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-03-12 13:47:38 -0500

    [+] App > useState: Query API & Update the State

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useState } from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
 /** Components */
 import Header from './components/Header';
 import SearchForm from './components/SearchForm';
@@ -6,7 +6,29 @@ import SearchForm from './components/SearchForm';
 function App() {
 
   /** Hook: Define State */
-  const [ category, setCategory ] = useState( '' );
+  const 
+    [ category, setCategory ] = useState( '' ),
+    [ news, setNews ] = useState( '' );
+
+  /** Hook: Tracking the state 'category' */
+  useEffect( () => {
+
+    const getApiData = async () => {
+
+        const
+            countryKey = 'co',      // Colombia
+            apiKey = '86c99ab8ccd742169c4cc39b5d28e0be',
+            url = `http://newsapi.org/v2/top-headlines?country=${ countryKey }&apiKey=${ apiKey }`,
+            response = await fetch( url ),
+            news = await response .json();
+
+            console .log( 'News', news );
+            setNews( news .articles );        // Asigna datos de respuesta de News API al State 'news'
+
+    }
+    getApiData();
+
+  }, [ category ] );
 
   return (
     <Fragment>
